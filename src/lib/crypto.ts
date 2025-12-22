111ebd6a76cd70b67d3c3837243d13fd2ecbe0b4
ByuN0-0 <devbyeon@gmail.com>
2025-12-22 22:01:18 +0900

    fix: salt type 정의

--- src/lib/crypto.ts
+++ src/lib/crypto.ts
@@ -11,7 +11,7 @@ export async function deriveKey(passphrase: string, salt: Uint8Array): Promise<C
   return window.crypto.subtle.deriveKey(
     {
       name: 'PBKDF2',
-      salt: salt,
+      salt: salt as BufferSource,
       iterations: 100000,
       hash: 'SHA-256',
     },
